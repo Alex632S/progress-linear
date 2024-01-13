5e7946a5dc1bf0ab80fbbaf140e1aa75cc77ba81
Alexandr Smotrov <98663387+Alex632S@users.noreply.github.com>
2024-01-13 21:56:44 +0300

    refactoring area

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,138 +1,189 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import IconsSvg from "../assets/IconsSvg.vue"
+import IconsSvg from "../assets/IconsSvg.vue";
 
 defineProps<{ msg: string }>();
 
 const count = ref(0);
 
-
-const S = ref(900)
-const n = ref(6)
-const p = ref(25)
-
+const S = ref(900);
+const n = ref(6);
+const p = ref(25);
 
 const params = {
-  _SumAll: String(S.value) + 'px',
-}
+  _SumAll: String(S.value) + "px",
+};
 
-const wS = ref(0)
-const width = ref()
+const wS = ref(0);
+const width = ref();
+
+const Area = (
+  _mark: string,
+  _value: number,
+  _pre: number,
+  _end: number,
+): void => {
+  if (
+    (count.value === _pre && _mark === "plus") ||
+    (count.value > _pre && count.value < _end) ||
+    (count.value === _end && _mark === "minus")
+  ) {
+    p.value = _value;
+  }
+};
 
 const result = (mark: string) => {
-  if ((count.value < 25) || (count.value === 25 && mark === 'minus')) {
-    p.value = 25
-    console.log('1')
-  }
-  if ((count.value === 25 && mark === 'plus') || (count.value > 25 && count.value < 50) || (count.value === 50 && mark === 'minus') ) {
-    p.value = 25 
-    console.log('2')
-  }
-  if ((count.value === 50 && mark === 'plus') || (count.value > 50 && count.value < 100) || (count.value === 100 && mark === 'minus')) {
-    p.value = 50 
-    console.log('3')
-  }
-  if ((count.value === 100 && mark === 'plus') || (count.value > 100 && count.value < 200) || (count.value === 200 && mark === 'minus')) {
-    p.value = 100 
-  }
-  if ((count.value === 200 && mark === 'plus') || (count.value > 200 && count.value < 500) || (count.value === 500 && mark === 'minus')) {
-    p.value = 300 
-  }
-  if ((count.value === 500 && mark === 'plus') || (count.value > 500 && count.value < 1000) || (count.value === 1000 && mark === 'minus')) {
-    p.value = 500 
-  }
-
-  if(mark === 'plus' && count.value < 1000) {
-    count.value += 1 
-    wS.value += (S.value / n.value) / p.value * (100 / S.value)
-  }
-
-  if(mark === 'minus' && count.value > 0) {
-    count.value -= 1 
-    wS.value -= (S.value / n.value) / p.value * (100 / S.value)
-  }
  
-  width.value = String(wS.value + '%')
-}
+  Area(mark, 25, 0, 25)
+  Area(mark, 25, 25, 50)
+  Area(mark, 50, 50, 100)
+  Area(mark, 100, 100, 200)
+  Area(mark, 300, 200, 500)
+  Area(mark, 500, 500, 1000)
+
+  if (mark === "plus" && count.value < 1000) {
+    count.value += 1;
+    wS.value += (S.value / n.value / p.value) * (100 / S.value);
+  }
+
+  if (mark === "minus" && count.value > 0) {
+    count.value -= 1;
+    wS.value -= (S.value / n.value / p.value) * (100 / S.value);
+  }
+
+  width.value = String(wS.value + "%");
+};
 </script>
 
 <template>
-  
   <div class="progress-linear-wrapper">
-
     <div class="mark-wrapper mark-wrapper--z-full mark-wrapper--flex">
       <div class="mark mark--relative mark--border">
         <span class="mark-value mark-value--top mark-value--right">
-          <IconsSvg :style="{color: count >= 25 ? '#3300FF' : 'currentColor'}" name-svg="star" width-svg="18px" height-svg="17px"></IconsSvg>
+          <IconsSvg
+            :style="{ color: count >= 25 ? '#3300FF' : 'currentColor' }"
+            name-svg="star"
+            width-svg="18px"
+            height-svg="17px"
+          ></IconsSvg>
         </span>
       </div>
 
       <div class="mark mark--relative mark--border">
         <span class="mark-value mark-value--top mark-value--right">
-          <IconsSvg :style="{color: count >= 50 ? '#3300FF' : 'currentColor'}" name-svg="star" width-svg="18px" height-svg="17px"></IconsSvg>
+          <IconsSvg
+            :style="{ color: count >= 50 ? '#3300FF' : 'currentColor' }"
+            name-svg="star"
+            width-svg="18px"
+            height-svg="17px"
+          ></IconsSvg>
         </span>
       </div>
       <div class="mark mark--relative mark--border">
         <span class="mark-value mark-value--top mark-value--right">
-          <IconsSvg :style="{color: count >= 100 ? '#3300FF' : 'currentColor'}" name-svg="star" width-svg="18px" height-svg="17px"></IconsSvg>
+          <IconsSvg
+            :style="{ color: count >= 100 ? '#3300FF' : 'currentColor' }"
+            name-svg="star"
+            width-svg="18px"
+            height-svg="17px"
+          ></IconsSvg>
         </span>
       </div>
       <div class="mark mark--relative mark--border">
         <span class="mark-value mark-value--top mark-value--right">
-          <IconsSvg :style="{color: count >= 200 ? '#3300FF' : 'currentColor'}" name-svg="star" width-svg="18px" height-svg="17px"></IconsSvg>
+          <IconsSvg
+            :style="{ color: count >= 200 ? '#3300FF' : 'currentColor' }"
+            name-svg="star"
+            width-svg="18px"
+            height-svg="17px"
+          ></IconsSvg>
         </span>
       </div>
       <div class="mark mark--relative mark--border">
         <span class="mark-value mark-value--top mark-value--right">
-          <IconsSvg :style="{color: count >= 500 ? '#3300FF' : 'currentColor'}" name-svg="star" width-svg="18px" height-svg="17px"></IconsSvg>
+          <IconsSvg
+            :style="{ color: count >= 500 ? '#3300FF' : 'currentColor' }"
+            name-svg="star"
+            width-svg="18px"
+            height-svg="17px"
+          ></IconsSvg>
         </span>
       </div>
       <div class="mark mark--relative mark--border">
         <span class="mark-value mark-value--top mark-value--right">
-          <IconsSvg :style="{color: count >= 1000 ? '#3300FF' : 'currentColor'}" name-svg="cup" width-svg="46.05px" height-svg="24.11px"></IconsSvg>
+          <IconsSvg
+            :style="{ color: count >= 1000 ? '#3300FF' : 'currentColor' }"
+            name-svg="cup"
+            width-svg="46.05px"
+            height-svg="24.11px"
+          ></IconsSvg>
         </span>
       </div>
     </div>
 
     <div class="mark-wrapper mark-wrapper--z-full mark-wrapper--flex">
       <div class="mark mark--relative mark--border">
-        <span class="mark-value mark-value--bottom mark-value--w-10 mark-value--left">0</span>
-        <span class="mark-value mark-value--bottom mark-value--w-60 mark-value--right">
-          <span v-if="count <= 25">{{ count + ' /' }}</span>  25</span>
+        <span
+          class="mark-value mark-value--bottom mark-value--w-10 mark-value--left"
+          >0</span
+        >
+        <span
+          class="mark-value mark-value--bottom mark-value--w-60 mark-value--right"
+        >
+          <span v-if="count <= 25">{{ count + " /" }}</span> 25</span
+        >
       </div>
 
       <div class="mark mark--relative mark--border">
-        <span class="mark-value mark-value--bottom mark-value--w-60 mark-value--right">
-          <span v-if="count > 25 && count <= 50">{{ count + ' /' }}</span> 50</span>
+        <span
+          class="mark-value mark-value--bottom mark-value--w-60 mark-value--right"
+        >
+          <span v-if="count > 25 && count <= 50">{{ count + " /" }}</span>
+          50</span
+        >
       </div>
       <div class="mark mark--relative mark--border">
-        <span class="mark-value mark-value--bottom mark-value--w-60 mark-value--right">
-          <span v-if="count > 50 && count <= 100">{{ count + ' /' }}</span> 100</span>
+        <span
+          class="mark-value mark-value--bottom mark-value--w-60 mark-value--right"
+        >
+          <span v-if="count > 50 && count <= 100">{{ count + " /" }}</span>
+          100</span
+        >
       </div>
       <div class="mark mark--relative mark--border">
-        <span class="mark-value mark-value--bottom mark-value--w-60 mark-value--right">
-          <span v-if="count > 100 && count <= 200">{{ count + ' /' }}</span> 200</span>
+        <span
+          class="mark-value mark-value--bottom mark-value--w-60 mark-value--right"
+        >
+          <span v-if="count > 100 && count <= 200">{{ count + " /" }}</span>
+          200</span
+        >
       </div>
       <div class="mark mark--relative mark--border">
-        <span class="mark-value mark-value--bottom mark-value--w-60 mark-value--right">
-          <span v-if="count > 200 && count <= 500">{{ count + ' /' }}</span> 500</span>
+        <span
+          class="mark-value mark-value--bottom mark-value--w-60 mark-value--right"
+        >
+          <span v-if="count > 200 && count <= 500">{{ count + " /" }}</span>
+          500</span
+        >
       </div>
       <div class="mark mark--relative mark--border">
         <span class="mark-value mark-value--bottom mark-value--right">
-          <span v-if="count > 500 && count < 1000">{{ count + ' /' }}</span> 1000</span>
+          <span v-if="count > 500 && count < 1000">{{ count + " /" }}</span>
+          1000</span
+        >
       </div>
     </div>
 
     <div class="progress-linear">
-        <div class="progress-linear__inner">
-          <div class="scale"></div>
-        </div>
+      <div class="progress-linear__inner">
+        <div class="scale"></div>
       </div>
+    </div>
   </div>
 
   <div class="mark-wrapper--flex">
-    <button style="margin-top: 70px;" @click="result('minus')">-</button>
-    <button style="margin-top: 70px;" @click="result('plus')">+</button>
+    <button style="margin-top: 70px" @click="result('minus')">-</button>
+    <button style="margin-top: 70px" @click="result('plus')">+</button>
   </div>
 
   <div class="card">
@@ -149,7 +200,7 @@ const result = (mark: string) => {
 }
 
 .progress-linear {
-  width: v-bind('params._SumAll');
+  width: v-bind("params._SumAll");
   height: 40px;
 
   position: relative;
@@ -168,70 +219,66 @@ const result = (mark: string) => {
 .scale {
   position: absolute;
 
-  background-color: #3300FF;
-  width: v-bind('width');
+  background-color: #3300ff;
+  width: v-bind("width");
   height: 100%;
-
 }
 
-  .mark-wrapper {
-    position: absolute;
+.mark-wrapper {
+  position: absolute;
 
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
+}
 
-  }
+.mark-wrapper--z-full {
+  z-index: 9999;
+}
+.mark-wrapper--flex {
+  display: flex;
+}
+.mark {
+  flex: 1 1 auto;
 
-  .mark-wrapper--z-full {
-    z-index: 9999;
-  }
-  .mark-wrapper--flex {
-    display: flex;
-  }
-  .mark {
-    flex: 1 1 auto;
+  height: 100%;
+}
+.mark--border:not(:last-child) {
+  border-right: solid 0.2px rgba(179, 179, 179, 1);
+}
+.progress-linear-wrapper {
+  position: relative;
+}
+.mark-value {
+  position: absolute;
+  text-align: center;
+  transform: translateX(50%);
+}
 
-    height: 100%;
-  }
-  .mark--border:not(:last-child) {
-    border-right: solid 0.2px rgba(179, 179, 179, 1);
-  }
-  .progress-linear-wrapper {
-    position: relative;
-  }
-  .mark-value {
-    position: absolute;
-    text-align: center; 
-    transform: translateX(50%);
-  }
-  
-  .mark-value--bottom {
-    bottom: -29px; 
-  }
+.mark-value--bottom {
+  bottom: -29px;
+}
 
-  .mark-value--top {
-    top: -29px; 
-  }
+.mark-value--top {
+  top: -29px;
+}
 
-  .mark-wrapper .mark:last-child .mark-value{
-    transform: translateX(0%);
-  }
-  .mark--relative {
-    position: relative;
-  }
+.mark-wrapper .mark:last-child .mark-value {
+  transform: translateX(0%);
+}
+.mark--relative {
+  position: relative;
+}
 
-  .mark-value--w-10 {
-    width: 15px; 
-  }
-  .mark-value--w-60 {
-    width: 80px; 
-  }
-  .mark-value--right {
-    right: 0; 
-  }
-  .mark-value--left {
-    left: 0; 
-  }
-
- 
+.mark-value--w-10 {
+  width: 15px;
+}
+.mark-value--w-60 {
+  width: 80px;
+}
+.mark-value--right {
+  right: 0;
+}
+.mark-value--left {
+  left: 0;
+}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import svgLoader from 'vite-svg-loader'
+import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    svgLoader({svgoConfig: {plugins: [{ name: 'prefixIds', params: { prefix: 'svg_loader_prefix_' } }] }}),
+    svgLoader({
+      svgoConfig: {
+        plugins: [
+          { name: "prefixIds", params: { prefix: "svg_loader_prefix_" } },
+        ],
+      },
+    }),
   ],
 });
